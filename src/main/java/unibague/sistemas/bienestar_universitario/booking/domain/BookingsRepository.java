02b5d7a40792c36496254a559ee32b948eb4413a
cristian999155 <cristian999155@gmail.com>
2022-10-24 22:39:40 -0500

    se subio el proyecto a git

---
+++ src/main/java/unibague/sistemas/bienestar_universitario/booking/domain/BookingsRepository.java
@@ -0,0 +1,31 @@
+package unibague.sistemas.bienestar_universitario.booking.domain;
+
+
+import org.springframework.data.jpa.repository.Query;
+import unibague.sistemas.bienestar_universitario.booking.infrastructure.entities.BookingEntity;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingsRepository {
+    void save(Booking booking);
+    Optional<BookingEntity> findBookingById(Long bookingId);
+    Optional<List<BookingEntity>> findBookingByDate(Calendar date);
+
+
+
+    Optional<List<BookingEntity>> findBookingByDate(Date dateStart, Date dateEnd);
+
+    Optional<List<BookingEntity>> findAllAngular();
+
+    Optional<List<BookingEntity>> findBookingByPersonId(Long personId);
+
+
+
+    Optional<List<BookingEntity>> findBookingByOfferedServiceId(Long offeredServiceId);
+    Optional<List<BookingEntity>> findBookingByPersonIdAndDate(Long personId, Calendar date);
+    List<BookingEntity> getAll();
+    void deleteById(BookingEntity bookingEntity);
+}
---
+++ src/main/java/unibague/sistemas/bienestar_universitario/booking/infrastructure/BookingRepositoryJpa.java
@@ -0,0 +1,36 @@
+package unibague.sistemas.bienestar_universitario.booking.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import unibague.sistemas.bienestar_universitario.booking.infrastructure.entities.BookingEntity;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingRepositoryJpa extends JpaRepository<BookingEntity, Long> {
+
+    @Query(value = "select * from reservas, persona,offered_service\n" +
+            "where reservas.personaid = persona.id  and reservas.servicioofertadoid = offered_service.id", nativeQuery = true)
+
+    Optional<List<BookingEntity>> findAllAngular();
+    @Query(value = "select * from reservas, persona,offered_service\n" +
+            "where reservas.personaid = persona.id  and reservas.servicioofertadoid = offered_service.id\n" +
+            "and cast(reservas.date as date) between ?1 and ?2 order by date ASC", nativeQuery = true)
+    Optional<List<BookingEntity>> findByDate(Date dateStar, Date dateEnd);
+
+
+    Optional<List<BookingEntity>> findByPersonId(Long personId);
+
+
+    @Query(value = "select * from reservas, persona,offered_service\n" +
+            "where cast(reservas.date as date) = CURRENT_DATE and reservas.personaid = persona.id  and reservas.servicioofertadoid = offered_service.id \n" +
+            "and reservas.servicioofertadoid = ?1 order by date ASC", nativeQuery = true)
+
+
+    Optional<List<BookingEntity>> findByOfferedServiceId(Long offeredServiceId);
+
+    Optional<List<BookingEntity>> findByPersonIdAndDate(Long personId, Calendar date);
+    void deleteByPersonId(BookingEntity bookingEntity);
+}
